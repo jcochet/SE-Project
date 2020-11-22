477a6eee92c0031cc9aa22a7f96a9ed17b16c60b
Sarah Ababou <sarah.ababou@etudiant.univ-rennes1.fr>
2020-11-22 17:01:23 +0100

    termite

--- src/App/Termite.java
+++ src/App/Termite.java
@@ -1,45 +1,56 @@
 package App;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Termite {
-	
     private int posX, posY;
 
     public Termite(int posX, int posY) {
-    	setPos(posX, posY);
+        this.posX=posX;
+        this.posY=posY;
+    }
+    //methodes
+    public int getPosX() {
+        return posX;
+    }
+    public int getPosY(){
+        return posY;
+    }
+    public Pair<Integer> getPosition(){
+      return new Pair<Integer>(posX,posY);
+    }
+    public void move (int positionX, int positionY){
+        this.posY=positionY;
+        this.posX=positionX;
     }
 
-    public List<Termite> findNeighbors(List<Termite> termites, int radius) {
-    	List<Termite> neighbors = new ArrayList<>();
-        for (Termite t: termites) {
-            if (isNeighbor(t.getPosX(), t.getPosY(), radius)) {
+    Set <Termite> findNeighbor (Set <Termite> termites , int radius ){
+        Set<Termite> neighbors = new HashSet <>();
+        for (Termite t: termites ) {
+            if ( isNeighbor(t.getPosX(),t.getPosY(),radius)){
                 neighbors.add(t);
             }
         }
         return neighbors;
     }
 
-    private boolean isNeighbor(int x, int y, int r) {
-        if ((x <= posX - r) && (x >= posX + r) && (y <= posY - r) && (y >= posY + r)) {
+    private boolean isNeighbor ( int x ,int y ,int r ){
+        if ((x <= posX - r) && (x >=posX +r) &&(y <= posY - r) && (y >= posY +r)){
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
-    
-    public int getPosX() {
-        return posX;
-    }
-    
-    public int getPosY(){
-        return posY;
-    }
-    
-    public void setPos(int posX, int posY) {
-        this.posX = posX;
-        this.posY = posY;
+    private class Pair<T>{
+        private T a,b;
+        public Pair(T a, T b){
+            this.a=a;
+            this.b=b;
+        }
+        @Override
+        public String toString(){
+            return "( "+a+" , "+b+" )";
+        }
     }
 
 }
